4a835719e156c6d3f170b883b47de8520eade9aa
Savio Gama <savio.gama09@gmail.com>
2021-05-14 12:02:48 -0300

    Componentes de botão, footer e página home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Image, Text } from './styles';
+import HomeButton from '../../components/HomeButton';
+import Footer from '../../components/Footer';
 
 export default function Home() {
     return (
-        <View />
-    )
+        <View>
+            <Image source={require('../../assets/logo_mapeamento_2.png')} />
+            <Text>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos,
+            e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os
+                embaralhou para fazer um livro de modelos de tipos.</Text>
+            <HomeButton color={'#4cb4d4'} icon={'bezier-curve'} text={'Conheça Paulo Freire'} />
+            <HomeButton color={'#7cc44c'} icon={'map-marked-alt'} text={'Mapa'} />
+            <HomeButton color={'#fbc30f'} icon={'list-alt'} text={'Solicitar cadastro'} />
+            <HomeButton color={'#f38424'} icon={'sign-out-alt'} text={'Sair do aplicativo'} />
+            <Footer />
+        </View>
+    );
 }
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components/native';
+
+export const View = styled.View`
+    flex: 1;
+    align-items: center;
+    margin-top: 10px;
+`;
+
+export const Text = styled.Text`
+    font-size: 14px;
+    text-align: center;
+    margin-horizontal: 15px;
+    margin-top: 3px;
+    margin-bottom: 15px;
+`;
+
+export const Image = styled.Image`
+    height: 120px;
+    width: 250px;
+    margin-left: 5px;
+    margin-right: 5px;
+`;
