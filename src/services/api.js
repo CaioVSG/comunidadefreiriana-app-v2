b84449b631bb7abb3f7e89d19d697e7b32c82b7b
Caio <caio.vsg@hotmail.com>
2021-12-07 11:28:12 -0300

    Endereço API

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://sistemas.ufape.edu.br/comunidadefreiriana/api/'
+    baseURL: 'http://185.28.23.76/login'
 });
 
 export default api;
